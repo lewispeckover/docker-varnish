def5e6d261306e9f5c0dc89ddf65295ab991890b
Lewis Peckover <lew@lew.io>
2016-10-06 21:27:00 +0700

    volume path needs to be absolute

--- vmod-build.sh
+++ vmod-build.sh
@@ -6,4 +6,4 @@ CMDS="apk add --no-cache --repository http://nl.alpinelinux.org/alpine/edge/main
       git clone -b 4.1 https://github.com/nigoroll/libvmod-dynamic.git && cd libvmod-dynamic;
       PKG_CONFIG_PATH=/usr/lib/pkgconfig ./autogen.sh && ./configure && make;
       echo '127.0.0.1 www.localhost img.localhost' >> /etc/hosts && make check"
-docker run -it --rm -v vmods:/vmods lewispeckover/alpine-builder sh -c "$CMDS"
+docker run -it --rm -v `pwd`/vmods:/vmods lewispeckover/alpine-builder sh -c "$CMDS"
