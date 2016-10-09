0854ab3a5e40a09e3b4a9f8f4a13c882cfb14bd7
Lewis Peckover <lew@lew.io>
2016-10-09 16:21:48 +0700

    fix args

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ case $1 in
 		# clear old varnish stuff
 		rm -rf /var/lib/varnish/*
 		ln -s /var/lib/varnish/`hostname` /var/lib/varnish/varnish
-		exec /usr/sbin/varnishd $*
+		exec /usr/sbin/varnishd "$@"
 		;;
 	varnishd | /usr/sbin/varnishd)
-		exec $*
+		exec "$@"
 		;;
 	varnish* | /usr/bin/varnish*)
 	  	TRIES=0
@@ -18,10 +18,10 @@ case $1 in
 	  	while [ ! -f /var/lib/varnish/varnish/_.vsm ]; do
 			if [ $(expr $TRIES + 1) -eq 5 ]; then
 				echo "Timed out waiting for a vsm file to appear"
-				exec $*
+				exec "$@"
 			fi
 			sleep 1
 		done
 		;;
 esac
-exec $*
+exec "$@"
