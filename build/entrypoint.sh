ba853322ac2f8eae14c8803085d467810ac3cc73
Lewis Peckover <lew@lew.io>
2017-02-27 23:43:27 +0800

    add modules

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/dumb-init /bin/sh
 set -e
 VARNISH_VERSION=4.1.5
+MODULES=https://github.com/nigoroll/libvmod-dynamic.git
 
 # install build deps
-apk add --no-cache pcre-dev ncurses-dev libedit-dev py-docutils linux-headers gcc libc-dev libgcc
+apk add --no-cache	autoconf automake libtool m5 \
+			pcre-dev ncurses-dev libedit-dev py-docutils linux-headers gcc libc-dev libgcc
 
 
 # fetch & extract
@@ -31,13 +33,18 @@ CPPFLAGS="-Os -fomit-frame-pointer" CC="gcc"
 # build it
 su-exec builder ./configure --build=x86_64-alpine-linux-musl --host=x86_64-alpine-linux-musl --prefix=/usr --sysconfdir=/etc --mandir=/usr/share/man --infodir=/usr/share/info --localstatedir=/var/lib --without-jemalloc
 su-exec builder make
-su-exec builder make check
 
 # install
 su-exec builder make DESTDIR="/build/dist" install
-#install -d -o varnish -g varnish -m750 \
-#       "$pkgdir"/var/cache/varnish \
-#       "$pkgdir"/var/log/varnish \
-#       "$pkgdir"/var/lib/varnish 
-#install -d -o root -g varnish -m750 "$pkgdir"/etc/varnish || return 1
 
+# modules..
+cd /build/src
+for MODULE in $MODULES; do 
+	M=basename $MODULE
+	git clone $MODULE
+	cd $GIT
+	[ -x ./autogen.sh ] && su-exec builder ./autogen.sh
+	[ -x ./configure ] && su-exec builder ./comfigure --prefix=/usr --sysconfdir=/etc --mandir=/usr/share/man --infodir=/usr/share/info --localstatedir=/var/lib --without-jemalloc
+       	su-exec builder make
+	su-exec builder make DESTDIR="/build/dist" install
+done
